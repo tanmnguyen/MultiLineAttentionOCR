20d3ccd163989f524c0cfe951b11222ff662beb8
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2023-11-18 20:20:15 -0500

    fix bug

--- dataset/OCRZippedDataset.py
+++ dataset/OCRZippedDataset.py
@@ -32,7 +32,7 @@ class OCRZippedDataset(Dataset):
         self.lbls      = lbls
 
     def __len__(self):
-g        assert len(self.img_paths) == len(self.lbls)
+        assert len(self.img_paths) == len(self.lbls)
         return len(self.img_paths)
 
     def __getitem__(self, idx):
